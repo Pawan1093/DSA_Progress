95860eedd2906adfd6136575440167fdb0bf3f98
Pawan Pawar <pawanpawar1093@gmail.com>
2026-09-09 11:52:57 +0530

    2 09-092026

---
+++ Recurssion_2/arrayRecursion/findMaxarray.py
@@ -0,0 +1,13 @@
+# def findmax(arr,i):
+#     if i == len(arr) - 1:
+#         return arr[i]
+
+
+#     return max(arr[i], findmax(arr, i+1))
+
+
+
+
+arr = [7, 8, 9, 1, 2]
+print(len(arr))
+# print(findmax(arr,0))
