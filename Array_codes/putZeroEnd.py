ec391d23732314afe1f6dc47229c27dcad368c58
Pawan Pawar <pawanpawar1093@gmail.com>
2026-09-20 16:41:06 +0530

    2 20-09-2026

--- Array_codes/putZeroEnd.py
+++ Array_codes/putZeroEnd.py
@@ -2,15 +2,15 @@ def zeroend(arr):
 
     i = 0
 
-    for j in range(1, len(arr)):
+    for j in range(len(arr)):
 
-        if arr[i] == 0:
-
+        if arr[j] != 0:
             arr[i], arr[j] = arr[j], arr[i]
-
-
-        i+=1
+            i += 1
 
     return arr
 
-print(zeroend([0, 2, 0 , 3, 5, 6, 8]))
+
+arr = [0, 2, 0, 3, 5, 6, 8]
+
+print(zeroend(arr))
